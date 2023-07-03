2256a7693c9ce1785131c648ff0e218733809d3b
DiegoFischerDev <diegofischer.dev@gmail.com>
2023-07-03 16:19:09 +0100

    removed hook useTheme

--- src/context/DarkThemeContext.js
+++ src/context/DarkThemeContext.js
@@ -1,13 +1,41 @@
 'use client';
 
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 
 export const DarkThemeContext = createContext({})
 
 export const DarkThemeContextProvider = ({ children }) => {
 
-  const [theme, setTheme] = useState(window.localStorage.getItem("@theme"));
+  const [theme, setTheme] = useState(null);
+  const preferDarkQuery = "(prefer-color-scheme: dark)"
+
+  useEffect(() => {
+
+    const mediaQuery = window.matchMedia(preferDarkQuery);
+    const userPref = window.localStorage.getItem("@theme");
+
+    if (userPref) {
+      setTheme(userPref)
+    } else {
+      let check = mediaQuery.matches ? "dark" : "light";
+      setTheme(check);
+    }
+
+  }, [])
+
+  useEffect(() => {
+
+    if (theme === "dark") {
+      window.localStorage.setItem("@theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      window.localStorage.setItem("@theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+
+  }, [theme])
+
 
   return (
     <DarkThemeContext.Provider value={{
